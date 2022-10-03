556b531dcbe7e3cc3cf5d31610d4e4f38c51763b
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2022-10-03 08:58:14 +0200

    All first numbers

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿static bool CzyPierwsza(int liczba)
+{
+    for (int i = 2; i <= liczba / 2; i++)
+    {
+        if (liczba % i == 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+int n = int.Parse(Console.ReadLine());
+for (int i = 2; i < n; i++)
+{
+    bool result = CzyPierwsza(i);
+    if (result)
+        Console.WriteLine(i);
+}
+
