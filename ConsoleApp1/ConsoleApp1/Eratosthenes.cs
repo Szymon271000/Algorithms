8c06c0732dd540ed6125692af29c0d3a37acdadf
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2022-10-03 10:11:40 +0200

    Refactor

---
+++ ConsoleApp1/ConsoleApp1/Eratosthenes.cs
@@ -0,0 +1,72 @@
+﻿
+
+namespace ConsoleApp1
+{
+    public class Eratosthenes
+    {
+        public List<int> firstNumbers { get; set; }
+
+        public Eratosthenes(List<int> firstNumbers)
+        {
+            this.firstNumbers = firstNumbers;
+        }
+
+        public bool CzyPierwsza(int liczba)
+        {
+            for (int i = 2; i <= liczba / 2; i++)
+            {
+                if (liczba % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public void CheckAndShowingFirstNumbers(int n)
+        {
+            for (int i = 3; i < n; i++)
+            {
+                bool result = CzyPierwsza(i);
+                if (result)
+                {
+                    firstNumbers.Add(i);
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        public void CreatingAndShowingPairs()
+        {
+            for (int i = 0; i < firstNumbers.Count; i++)
+            {
+                bool result = CzyPierwsza(firstNumbers[i]);
+                int secondFirstNumber = firstNumbers[i] + 2;
+                bool secondResult = CzyPierwsza(secondFirstNumber);
+                if (result && secondResult)
+                {
+                    Console.WriteLine($"{secondFirstNumber - 2} {secondFirstNumber}");
+                }
+            }
+        }
+
+        public void CreatingAndShowingQUarters()
+        {
+            for (int i = 0; i < firstNumbers.Count; i++)
+            {
+                bool result = CzyPierwsza(firstNumbers[i]);
+                int secondFirstNumber = firstNumbers[i] + 2;
+                int thirdFirstNumber = firstNumbers[i] + 6;
+                int fourthFirstNumber = firstNumbers[i] + 8;
+                bool secondResult = CzyPierwsza(secondFirstNumber);
+                bool thirdResult = CzyPierwsza(thirdFirstNumber);
+                bool fourthResult = CzyPierwsza(fourthFirstNumber);
+                if (result && secondResult && thirdResult && fourthResult)
+                {
+                    Console.WriteLine($"{secondFirstNumber - 2} {secondFirstNumber} {thirdFirstNumber} {fourthFirstNumber}");
+                }
+            }
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
-﻿static bool CzyPierwsza(int liczba)
+﻿using ConsoleApp1;
+
+static bool CzyPierwsza(int liczba)
 {
     for (int i = 2; i <= liczba / 2; i++)
     {
@@ -12,40 +14,10 @@
 }
 
 List<int> firstNumbers = new List<int>();
+Eratosthenes eratosthenes = new Eratosthenes(firstNumbers);
 int n = int.Parse(Console.ReadLine());
-for (int i = 3; i < n; i++)
-{
-    bool result = CzyPierwsza(i);
-    if (result)
-    {
-        firstNumbers.Add(i);
-        Console.WriteLine(i);
-    }
-}
-
+eratosthenes.CheckAndShowingFirstNumbers(n);
 Console.WriteLine("-------------------------------");
-for (int i = 0; i < firstNumbers.Count; i++)
-{
-    bool result = CzyPierwsza(firstNumbers[i]);
-    int secondFirstNumber = firstNumbers[i] + 2;
-    bool secondResult = CzyPierwsza(secondFirstNumber);
-    if (result && secondResult)
-    {
-        Console.WriteLine($"{secondFirstNumber - 2} {secondFirstNumber}");
-    }
-}
+eratosthenes.CreatingAndShowingPairs();
 Console.WriteLine("-------------------------------");
-for (int i = 0; i < firstNumbers.Count; i++)
-{
-    bool result = CzyPierwsza(firstNumbers[i]);
-    int secondFirstNumber = firstNumbers[i] + 2;
-    int thirdFirstNumber = firstNumbers[i] + 6;
-    int fourthFirstNumber = firstNumbers[i] + 8;
-    bool secondResult = CzyPierwsza(secondFirstNumber);
-    bool thirdResult = CzyPierwsza(thirdFirstNumber);
-    bool fourthResult = CzyPierwsza(fourthFirstNumber);
-    if (result && secondResult && thirdResult && fourthResult)
-    {
-        Console.WriteLine($"{secondFirstNumber - 2} {secondFirstNumber} {thirdFirstNumber} {fourthFirstNumber}");
-    }
-}
+eratosthenes.CreatingAndShowingQUarters();
